ce8d276d29a918278d27a8cf563b5cf64250c395
Ross <Ross@myyearofcode.com>
2019-01-15 15:27:29 -0500

    added rspec tests

--- snow-shoveler/code.rb
+++ snow-shoveler/code.rb
@@ -1,41 +1,97 @@
-# 20190114
-width = 10
-length = 4.95
-depth = 12 # depth is measured in inches
-price = nil
-depth /= 12.0 # depth comverted to feet (float)
-cli = false
+# 20190115
+require 'rspec/autorun'
 
-# OPTIONAL cli interface
-if cli
-  puts('Please enter your length in feet.')
-  length = gets.chomp.to_f
-  puts('Please enter your width in feet.')
-  width = gets.chomp.to_f
-  puts('Please enter your depth in inches.')
-  depth = gets.chomp.to_f
+class Shoveling_Quote
+
+  def get_input
+    # user input validated to be numeric
+    begin
+      puts('Please enter your length in feet.')
+      length = Float(gets.chomp)
+    rescue
+        puts('Try again. That wasn\'t a number')
+        retry
+    end
+
+    begin
+      puts('Please enter your width in feet.')
+      width = Float(gets.chomp)
+    rescue
+        puts('Try again. That wasn\'t a number')
+        retry
+    end
+
+    begin
+      puts('Please enter your depth in inches.')
+      depth = Float(gets.chomp)
+    rescue
+      puts('Try again. That wasn\'t a number')
+      retry
+    end
+
+    return length, width, depth
+  end
+
+  def calculate_area(length, width, depth)
+    # return volume of snow  - only needed for testing
+    cubicArea = (width * depth * length).ceil #round up. Greedy martian shovelers...
+    puts('Cubic volume:')
+    puts(cubicArea.to_s + " Cubic Feet")
+    return cubicArea
+  end
+
+  def make_quote(cubicArea)
+    # Switch statement! Return price from pricing structure
+    case cubicArea
+      when 0..49
+        price =  20
+      when 50..149
+        price =  50
+      when 150..299
+        price = 100
+      else
+        price = 150
+    end
+    return price
+  end
+
+  def output_quote(price)
+    # return price to console
+    puts("Price:")
+    puts format("$%.2f",price)
+  end
+
+  def run
+    # this is where the magic happens ;)
+    length, width, depth = get_input
+    cubicArea = calculate_area(length, width, depth)
+    price = make_quote(cubicArea)
+    output_quote(price)
+  end
 end
 
-cubicArea = (width * depth * length).ceil #round up. Greedy martian shovelers...
-puts('Cubic volume:')
-puts(cubicArea.to_s + " Cubic Feet")
+myQuote = Shoveling_Quote.new
+myQuote.run
 
-# Switch statement!
-case cubicArea
-  when 0..49
-    price =  20
-  when 50..149
-    price =  50
-  when 150..299
-    price = 100
-  else
-    price = 150
+# TESTING
+describe Shoveling_Quote do
+  it 'should output a price to console' do
+    myQuote = Shoveling_Quote.new
+    expect {myQuote.output_quote(10)}.to output("Price:\n$10.00\n").to_stdout
+  end
+
+  it 'should return 20 when given 40' do
+    myQuote = Shoveling_Quote.new
+    expect(myQuote.make_quote(40)).to eq(20)
+  end
+
+  it 'should return 120 when given 10,6,24' do
+    myQuote = Shoveling_Quote.new
+    expect(myQuote.calculate_area(10,6,2)).to eq(120)
+  end
+
+  it 'should return 120 when given 10,6,24' do
+    myQuote = Shoveling_Quote.new
+    expect(myQuote.calculate_area(10,6,2)).to_not eq(121)
+  end
 end
-
-puts("Price:")
-puts format("$%.2f",price)
-
-  # 0 - 49 cubic ft:  $20
-  # 50 - 149 cubic ft: $50
-  # 150 - 299 cubic ft: $100
-  # 300+ cubic ft: $150
