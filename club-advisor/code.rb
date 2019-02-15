ef86a029578ef9e8e4db51fd49dbf3837e9a2bfa
Ross <Ross@myyearofcode.com>
2019-02-14 22:47:16 -0500

    Launch Academy Ignition week 5 work

---
+++ club-advisor/code.rb
@@ -0,0 +1,26 @@
+clubs = {
+  driver: 200,
+  four_wood: 180,
+  five_wood: 175,
+  two_iron: 170,
+  three_iron: 160,
+  four_iron: 150,
+  five_iron: 140,
+  six_iron: 130,
+  seven_iron: 120,
+  eight_iron: 110,
+  nine_iron: 95,
+  pitching_wedge: 80,
+  sand_Wedge: 20,
+  putter: 0
+}
+
+puts "How far away are you?"
+away = gets.chomp()
+possibleClubs = []
+clubs.each do |club, distance|
+  if distance > away.to_i
+    possibleClubs.push(club)
+  end
+end
+puts "You should use the #{possibleClubs.last.to_s.gsub('_',' ')}!"
