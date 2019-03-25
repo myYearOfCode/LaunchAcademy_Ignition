9b88c49fb4cb32562cab09b44ce6e39b0c4eb7d1
Ross <Ross@myyearofcode.com>
2019-03-24 22:36:57 -0400

    repeating letter, endsWithVowel, and captureThreeNumbers

--- dark-nebula-es6/js/src/core/regex.js
+++ dark-nebula-es6/js/src/core/regex.js
@@ -4,17 +4,19 @@ containsNumber = (string) => {
 };
 
 containsRepeatingLetter = (string) => {
-  let regex = /([a-zA-Z])\1{2,}/
+  let regex = /([a-zA-Z])\1{1,}/
   console.log(string + " " + regex.test(string))
   return regex.test(string)
 };
 
 endsWithVowel = (string) => {
-
+  let regex = /[aeiou]$/i
+  return regex.test(string)
 };
 
 captureThreeNumbers = (string) => {
-
+  let regex = /(\d){3}/
+  return regex.test(string) ? string.match(regex)[0] : false
 };
 
 matchesPattern = (string) => {
