2b41cd1247641e2e463892ac3d4f3286a5dc8583
Ross <Ross@myyearofcode.com>
2019-04-05 20:53:43 -0400

    getting back into js

--- dark-nebula-es6/js/src/core/regex.js
+++ dark-nebula-es6/js/src/core/regex.js
@@ -5,7 +5,7 @@ containsNumber = (string) => {
 
 containsRepeatingLetter = (string) => {
   let regex = /([a-zA-Z])\1{1,}/
-  console.log(string + " " + regex.test(string))
+  // console.log(string + " " + regex.test(string))
   return regex.test(string)
 };
 
@@ -20,9 +20,12 @@ captureThreeNumbers = (string) => {
 };
 
 matchesPattern = (string) => {
-
+  let regex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/
+  return regex.test(string)
 };
 
 isUSD = (string) => {
-
+  let regex = /^\$\d{1,3}(,\d{3})*((\.\d{2}$)|$){1}/
+  console.log( string + " " + regex.test(string))
+  return regex.test(string)
 };
--- dark-nebula-es6/js/src/core/strings.js
+++ dark-nebula-es6/js/src/core/strings.js
@@ -1,5 +1,14 @@
 reduceString = (str, amount) => {
-
+  let regex = /([a-zA-Z])\1*/gm
+  let results = str.search(regex)
+  console.log(results)
+  let output = results.map(substring => {
+    if (substring.length > amount) {
+      return substring.slice(1,(amount+1))
+    }
+    return substring
+  })
+  return output.join()
 };
 
 reverseString = (str) => {
